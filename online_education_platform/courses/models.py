c0f5f7a55f79795d9b5782ef7175f8e816357465
A. Serguta <107645881+anastasiaserguta@users.noreply.github.com>
2024-08-01 23:27:24 +0300

    Update models.py

--- online_education_platform/courses/models.py
+++ online_education_platform/courses/models.py
@@ -2,16 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import datetime
 from django.urls import reverse
-<<<<<<< HEAD
-# from uuid import uuid4
-
-# Модель курса, где title - название, description - описание, 
-# teacher - преподаватель, который его создал, created - дата создания.
-=======
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
->>>>>>> ced3057 (Pre-alpha v.0.1)
+
 class Course(models.Model): 
     course_title = models.CharField(max_length=64, null=True, help_text='название курса')
     course_description = models.TextField(max_length=128, null=True, help_text='описание курса')
@@ -24,15 +18,10 @@ class Course(models.Model):
     
     def get_absolute_url(self):
         return reverse('courses-detail', kwargs={'pk': self.pk})
-<<<<<<< HEAD
 
-# Класс модулей, из которых состоит курс, 
-# содержит объекты курса, название и описание.    
-=======
     
     
 
->>>>>>> ced3057 (Pre-alpha v.0.1)
 class Module(models.Model):
     module_course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, related_name='course_modules', default=None)
     module_title = models.CharField(max_length=200, null=True)
@@ -41,27 +30,15 @@ class Module(models.Model):
     def __str__(self):
         return self.module_title
     
-<<<<<<< HEAD
-=======
+
     def get_absolute_url(self):
         return reverse('module-detail', kwargs={'pk': self.pk})
->>>>>>> ced3057 (Pre-alpha v.0.1)
+
     
 class Task(models.Model):
     task_module = models.ForeignKey(Module, on_delete=models.DO_NOTHING, related_name='module_task', default=None)
     task_title = models.CharField(max_length=128)
     task_description = models.TextField(blank=True)
-<<<<<<< HEAD
-    task_file = models.FileField()
-    created_at = models.DateTimeField(auto_now_add=datetime)
-    update_at = models.DateTimeField(auto_now=datetime)
-    is_completed = models.BooleanField(default=False)
-    email = models.EmailField(null=True)
-    notes = models.IntegerField(null=True)
-
-    def __str__(self):
-        return self.task_title
-=======
     task_file = models.FileField(default=None, blank=True)
     created_at = models.DateTimeField(auto_now_add=datetime)
     update_at = models.DateTimeField(auto_now=datetime)
@@ -104,7 +81,7 @@ class Student(models.Model):
 
     
         
->>>>>>> ced3057 (Pre-alpha v.0.1)
+
     
 
 
