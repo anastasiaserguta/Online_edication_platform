78223b7cc9e36e0159bd08d6779b2533b37a0d13
Anastasia Serguta <anastasia.serguta@gmail.com>
2024-08-02 01:06:42 +0300

    remove project

--- online_education_platform/courses/apps.py
+++
@@ -1,8 +0,0 @@
-# from django import setup
-from django.apps import AppConfig
-
-# setup()
-
-class CoursesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'courses'
